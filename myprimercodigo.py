2c60b9c5ee633e4ca379067fb9fc41abe192c13c
Natalia <angelanataliarojaspaz@gmail.com>
2024-07-05 12:01:46 -0500

    nueva rama y suma

--- myprimercodigo.py
+++ myprimercodigo.py
@@ -1,1 +1,14 @@
 print ("hola mundo")
+# Función para sumar dos enteros
+def sumar_enteros(a, b):
+    return a + b
+
+# Solicitar al usuario que ingrese los números
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: "))
+
+# Calcular la suma
+resultado = sumar_enteros(numero1, numero2)
+
+# Imprimir el resultado
+print(f"La suma de {numero1} y {numero2} es {resultado}")
